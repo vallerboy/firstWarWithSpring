da526893ae69857091f6cb8cb13d56f816598571
Oskar Polak <biuro@fivarto.pl>
2018-03-08 15:12:16 +0100

    asdad

--- src/main/java/pl/oskarpolak/springdemostart/models/RegisterForm.java
+++ src/main/java/pl/oskarpolak/springdemostart/models/RegisterForm.java
@@ -2,12 +2,20 @@ package pl.oskarpolak.springdemostart.models;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Email;
+
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
 public class RegisterForm {
+    @Size(min = 3, max = 19)
     private String login;
+    @Pattern(regexp = "[a-z]+")
     private String password;
+    @Pattern(regexp = "[a-z]+")
     private String passwordRepeat;
+    @Email
     private String email;
 }
